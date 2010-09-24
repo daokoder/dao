5a88e8d0fc4a928cf7fc84bea37856aeadab267e
pompei2@gmail.com <pompei2@gmail.com>
2010-09-24 23:03:55 +0000

    Minor Fix: the compilation on non pure-glibc platforms other than msvc.

--- src/daoGC.c
+++ src/daoGC.c
@@ -2,12 +2,12 @@
   This file is a part of a virtual machine for the Dao programming language.
   Copyright (C) 2006-2010, Fu Limin. Email: fu@daovm.net, limin.fu@yahoo.com
 
-  This software is free software; you can redistribute it and/or modify it under the terms 
-  of the GNU Lesser General Public License as published by the Free Software Foundation; 
+  This software is free software; you can redistribute it and/or modify it under the terms
+  of the GNU Lesser General Public License as published by the Free Software Foundation;
   either version 2.1 of the License, or (at your option) any later version.
 
-  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Lesser General Public License for more details.
   =========================================================================================*/
 
@@ -32,7 +32,7 @@
 #endif
 #endif
 
-#ifndef _MSC_VER
+#ifdef __GLIBC__
 #include <execinfo.h>
 void print_trace()
 {
@@ -1767,10 +1767,10 @@ void directDecRC()
 				{
 					DaoRoutine *rout = (DaoRoutine*)dbase;
 					GC_BREAK_REF( rout->nameSpace );
-					/* may become NULL, if it has already become garbage 
+					/* may become NULL, if it has already become garbage
 					 * in the last cycle */
 					GC_BREAK_REF( rout->routType );
-					/* may become NULL, if it has already become garbage 
+					/* may become NULL, if it has already become garbage
 					 * in the last cycle */
 					GC_BREAK_REF( rout->routHost );
 
