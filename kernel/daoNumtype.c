4a7b4a4e43f40eb921d8050adce2fc27e9a8b3b6
Limin Fu <daokoder@gmail.com>
2012-04-29 20:52:06 +0000

    Change: from the LGPL license to the MIT license.

--- kernel/daoNumtype.c
+++ kernel/daoNumtype.c
@@ -1,15 +1,22 @@
-/*=========================================================================================
-  This file is a part of a virtual machine for the Dao programming language.
-  Copyright (C) 2006-2012, Fu Limin. Email: fu@daovm.net, limin.fu@yahoo.com
-
-  This software is free software; you can redistribute it and/or modify it under the terms 
-  of the GNU Lesser General Public License as published by the Free Software Foundation; 
-  either version 2.1 of the License, or (at your option) any later version.
-
-  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-  See the GNU Lesser General Public License for more details.
-  =========================================================================================*/
+/*
+// This file is part of the virtual machine for the Dao programming language.
+// Copyright (C) 2006-2012, Limin Fu. Email: daokoder@gmail.com
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+// software and associated documentation files (the "Software"), to deal in the Software 
+// without restriction, including without limitation the rights to use, copy, modify, merge, 
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or 
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #include"daoStream.h"
 #include"daoProcess.h"
