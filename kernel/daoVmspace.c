ce50456b5237db4cb30ee02d93852a563d761a26
Limin Fu <daokoder@gmail.com>
2014-11-17 09:49:55 +0000

    Fix: a typo.

--- kernel/daoVmspace.c
+++ kernel/daoVmspace.c
@@ -2643,7 +2643,7 @@ void DaoQuit()
 	DMutex_Destroy( & mutex_routine_specialize );
 	DMutex_Destroy( & mutex_routine_specialize2 );
 	DMutex_Destroy( & dao_cdata_mutex );
-	DaoInitThread();
+	DaoQuitThread();
 #endif
 }
 DaoNamespace* DaoVmSpace_FindModule( DaoVmSpace *self, DString *fname )
