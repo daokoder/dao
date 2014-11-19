83d83ad0194c868b8272fda73bd21089f5e6b6df
Limin Fu <daokoder@gmail.com>
2014-11-19 09:57:34 +0000

    Add: minor changes.

--- share/release.sh
+++ share/release.sh
@@ -8,10 +8,12 @@ fossil close
 mkdir -p doc
 cp -r ../doc/html doc/
 
-cd ../modules
+cd modules
 fossil open --nested ../../modules/DaoModules.fossil
 fossil close
 
 cd ../tools
 fossil open --nested ../../tools/DaoTools.fossil
 fossil close
+
+cd ../../ && tar -zcf Dao-$VERSION.tgz Dao-$VERSION
